7d4123a1956f8b805d13c641fb88d5571fada7e1
Eugeny Konstantinov <jskonst@yandex.ru>
2020-10-30 09:33:53 +0300

    Moved to T|S, added material ui (#16)

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+  );
+}
+
+export default App;
