806ec605235b3346e5ece9cdef3bb84dbd19abbd
Eugeny KOnstantinov <jskonst@yandex.ru>
2020-11-07 00:56:40 +0300

    Add constraints to initialization

--- client/src/Camera/Camera.tsx
+++ client/src/Camera/Camera.tsx
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Webcam from "react-webcam";
 
 const webgazer = require("webgazer");
 
 const videoConstraints = {
-  width: 320,
-  height: 240,
-  facingMode: "environment",
+  width: { min: 320, ideal: 1280, max: 1920 },
+  height: { min: 240, ideal: 720, max: 1080 },
+  facingMode: "user",
 };
 
 export default function Camera() {
+    const init = async () => {
+    await webgazer.default
+      .setGazeListener(function (data: any, elapsedTime: any) {
+        if (data == null) {
+          return;
+        }
+        var xprediction = data.x; //these x coordinates are relative to the viewport
+        var yprediction = data.y; //these y coordinates are relative to the viewport
+        console.log(elapsedTime); //elapsed time is based on time since begin was called
+        console.log("TEST");
+      })
+      .begin();
+    await webgazer.default.setCameraConstraints(videoConstraints);
+    webgazer.default.params.showVideoPreview = true;
+    console.log("Inited");
+    };
+  useEffect(() => {
+    init();
+  });
 
-    webgazer.default.setGazeListener(function(data :any, elapsedTime: any) {
-    if (data == null) {
-        return;
-    }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(elapsedTime); //elapsed time is based on time since begin was called
-    console.log("TEST");
-}).begin();
- webgazer.default.params.showVideoPreview = true;
-
-  return (
-    <>
-    </>
-  );
+  return <></>;
 }
